2a4c437c20c1d02f8b387a6892b4f2c70ce718ca
nathanwbrei <nathan.w.brei@gmail.com>
2016-04-27 21:12:54 +0200

    Rough cut of main.c

--- worksheet1/main.c
+++ worksheet1/main.c
@@ -1,7 +1,11 @@
 #include "helper.h"
 #include "visual.h"
 #include "init.h"
+#include "boundary_val.h"
+#include "uvp.h"
+#include "sor.h"
 #include <stdio.h>
+
 
 
 /**
@@ -38,5 +42,67 @@
  * - calculate_uv() Calculate the velocity at the next time step.
  */
 int main(int argn, char** args){
-  return -1;
+
+    // Define the problem parameters 
+    // Geometry parameters
+    double xlength, ylength, dx, dy;
+    int imax, jmax;
+    // Timestep parameters
+    double t, t_end, dt, tau, dt_value;
+    int n;
+    // Pressure iteration data
+    double eps, omg, alpha;
+    int itermax;
+    // Problem-dependent quantities
+    double Re, GX, GY, UI, VI, PI;
+    // Data structures
+    double **U, **V, **P, **RS, **F, **G;
+
+
+    // Read the problem parameters
+    read_parameters(args[0], &Re, &UI, &VI, &PI, &GX, &GY, &t_end, &xlength, 
+                    &ylength, &dt, &dx, &dy, &imax, &jmax, &alpha, &omg, &tau, 
+                    &itermax, &eps, &dt_value); 
+    n = 0;
+    t = 0;
+    // Assign initial values to u,v,p
+    U = matrix(0, imax+1, 0, jmax+1); //TODO: Are these the right indices?
+    V = matrix(0, imax+1, 0, jmax+1);
+    P = matrix(0, imax+1, 0, jmax+1);
+    F = matrix(0, imax+1, 0, jmax+1); //TODO: Definitely not the right indices
+    G = matrix(0, imax+1, 0, jmax+1);
+    RS = matrix(0, imax+1, 0, jmax+1);
+
+    init_uvp(UI, VI, PI, imax, jmax, U, V, P);
+
+    while (t < t_end) {
+        calculate_dt(Re,tau,&dt,dx,dy,imax,jmax,U,V); 
+        boundaryvalues(imax,jmax,U,V);
+        calculate_fg(Re,GX,GY,alpha,dt,dx,dy,imax,jmax,U,V,F,G);
+        calculate_rs(dt,dx,dy,imax,jmax,F,G,RS);
+        int it = 0;
+        double res = eps+1; // Residual starts off > eps
+        while (it < itermax && res > eps) {
+            sor(omg,dx,dy,imax,jmax,P,RS,&res);
+            it += 1;
+        }
+        calculate_uv(dt,dx,dy,imax,jmax,U,V,F,G,P);
+        //TODO: Condition this on some parameter
+        //TODO: When do we use this vs the write_matrix()?
+        write_vtkFile(args[0], n, xlength, ylength, imax, jmax, dx, dy, U, V, P);
+        t += dt;
+        n += 1;
+    }
+    // Output u,v,p for visualization
+    // TODO: Do we want this?
+    
+    // Free the matrices we've allocated
+    free_matrix(U, 0, imax+1, 0, jmax+1); //TODO: Are these the right indices?
+    free_matrix(V, 0, imax+1, 0, jmax+1);
+    free_matrix(P, 0, imax+1, 0, jmax+1);
+    free_matrix(F, 0, imax+1, 0, jmax+1); //TODO: Definitely the wrong indices
+    free_matrix(G, 0, imax+1, 0, jmax+1);
+    free_matrix(RS, 0, imax+1, 0, jmax+1);
+     
+    return -1;
 }
