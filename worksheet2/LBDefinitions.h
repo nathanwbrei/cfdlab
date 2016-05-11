4b1e90850f7d752b83eaaa86110af26ac88da7e8
nasay <nsay92@gmail.com>
2016-05-11 12:54:26 +0200

    LB Definitions

--- worksheet2/LBDefinitions.h
+++ worksheet2/LBDefinitions.h
@@ -1,9 +1,21 @@
+#include <math.h>
+
 #ifndef _LBDEFINITIONS_H_
 #define _LBDEFINITIONS_H_
 
-  static const int LATTICEVELOCITIES[19][3];
-  static const double LATTICEWEIGHTS[19];
-  static const double C_S;
+static const int LATTICEVELOCITIES[19][3] = {
+    {0, -1, -1}, {-1, 0, -1}, {0, 0, -1}, {1, 0, -1}, {0, 1, -1},
+    {-1, -1, 0}, {0, -1, 0}, {1, -1, 0}, {-1, 0, 0}, {0, 0, 0}, {1, 0, 0}, {-1, 1, 0}, {0, 1, 0}, {1, 1, 0},
+    {0, -1, 1}, {-1, 0, 1}, {0, 0, 1}, {1, 0, 1}, {0, 1, 1}
+};
+
+static const double LATTICEWEIGHTS[19] = {
+    1.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36,
+    1.0/36, 2.0/36, 1.0/36, 2.0/36, 12.0/36, 2.0/36, 1.0/36, 2.0/36, 1.0/36, 
+    1.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36
+};
+
+static const double C_S = 1.0 / sqrt(3.0);
 
 #endif
 
