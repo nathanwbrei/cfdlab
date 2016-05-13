075c267a0dc2f037fbb796b1fd38715cdc3717fe
nathanwbrei <nathan.w.brei@gmail.com>
2016-05-13 23:02:27 +0200

    Rough cut of collision.c
    Conflicts:
    	worksheet2/main.c

--- worksheet2/collision.c
+++ worksheet2/collision.c
@@ -1,10 +1,48 @@
+#include "helper.h"
 #include "collision.h"
 
 void computePostCollisionDistributions(double *currentCell, const double * const tau, const double *const feq){
-  /* TODO */
+    /* Compute the post-collision distribution f*[i] according to BGK update rule. See Eq 14.  */
+
+    int i;
+    double fi;
+
+    for (i=0; i<19; i++) {
+        fi = currentCell[i];
+        currentCell[i] = fi - (fi - feq[i]) / *tau;
+    }
 }
 
 void doCollision(double *collideField, int *flagField,const double * const tau,int xlength){
-  /* TODO */
+    /* 
+     * For each inner grid cell in collideField, compute the post-collide
+     * distribution
+     */
+ 
+    double density;
+    double velocity[3];
+    double feq[19];
+
+    double * currentCell;
+
+    int x,y,z,i;
+    int n = xlength + 2;
+
+    // Loop over inner cells: compare to streaming.c
+    for (x=1; x<n; x++) {
+        for (y=1; y<n; y++) {
+            for (z=1; z<n; z++) {
+                for (i=0; i<19; i++) {
+                    currentCell = getEl(collideField, x, y, z, i, xlength);
+                    computeDensity(currentCell, &density);
+                    computeVelocity(currentCell, &density, velocity);
+                    computeFeq(&density, velocity, feq);
+                    computePostCollisionDistributions(currentCell, tau, feq);
+                }
+            }
+        }
+    }
+    // TODO: Consider iterating over non-flagged boundaries-- How
+    // general do we want this?
 }
 
--- worksheet2/streaming.c
+++ worksheet2/streaming.c
@@ -4,13 +4,13 @@
 
 void doStreaming(double * collideField, double * streamField, int * flagField, int xlength){
     int x, y, z, i;
-    int n = xlength + 2;
+    int n = xlength + 2;  //TODO: Verify this
 
     /* Loop for inner cells */
     for (x = 1; x < n; x++) {
         for (y = 1; y < n; y++) {
             for (z = 1; z < n; z++) {
-                for (i = 1; i < 19; i++) {
+                for (i = 1; i < 19; i++) {  //TODO: Shouldn't i start at 0? // TODO: define Q in one place
                     *getEl(collideField, x, y, z, i, xlength) = *getEl(streamField, x - LATTICEVELOCITIES[i][0], y - LATTICEVELOCITIES[i][1], z - LATTICEVELOCITIES[i][2], i, xlength);
                 }
             }
