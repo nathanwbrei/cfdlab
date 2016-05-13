976ac7afbc8387457f748404f280e8370ed319fb
nasay <nsay92@gmail.com>
2016-05-13 17:46:22 +0200

    output density + velocity, spaces

--- worksheet2/visualLB.c
+++ worksheet2/visualLB.c
@@ -1,72 +1,76 @@
 #include "visualLB.h"
+#include "computeCellValues.h"
+#include "helper.h"
 
 void write_vtkFile(const char *szProblem,
                    int    t,
                    double xlength,
                    double *collideField) {
   
-    int x, y, z;
-    char szFileName[80];
-    FILE *fp=NULL;
-    int n = xlength + 2;
-    double velocity[3];
-    double dencity;
+  int x, y, z;
+  char szFileName[80];
+  FILE *fp=NULL;
+  int n = xlength + 2;
+  double velocity[3];
+  double density;
+  double * el = NULL;
 
-    sprintf( szFileName, "%s.%i.vtk", szProblem, t );
-    fp = fopen( szFileName, "w");
-    if( fp == NULL )		       
+  sprintf( szFileName, "%s.%i.vtk", szProblem, t );
+  fp = fopen( szFileName, "w");
+  if( fp == NULL )		       
     {
-        char szBuff[80];
-        sprintf( szBuff, "Failed to open %s", szFileName );
-        ERROR( szBuff );
-        return;
+      char szBuff[80];
+      sprintf( szBuff, "Failed to open %s", szFileName );
+      ERROR( szBuff );
+      return;
     }
 
-    write_vtkHeader(fp, xlength);
-    write_vtkPointCoordinates(fp, xlength);
+  write_vtkHeader(fp, xlength);
+  write_vtkPointCoordinates(fp, xlength);
 
-    fprintf(fp,"POINT_DATA %i \n", n*n*n);
+  fprintf(fp,"POINT_DATA %i \n", n*n*n);
   
-    fprintf(fp,"\n");
-    fprintf(fp, "VECTORS velocity float\n");
-    for(z = 0; z < n; z++) {
-        for(y = 0; y < n; y++) {
-            for(x = 0; x < n; x++) {
-                /* TODO compute velocity */
-                velocity[0] = 1.0;
-                velocity[1] = 1.5;
-                velocity[2] = 0.5;
-                fprintf(fp, "%f %f %f\n", velocity[0], velocity[1], velocity[2]);
-            }
-        }
-    }
+  fprintf(fp,"\n");
+  fprintf(fp, "VECTORS velocity float\n");
+  for(z = 0; z < n; z++) {
+    for(y = 0; y < n; y++) {
+      for(x = 0; x < n; x++) {
+        /* TODO compute velocity */
+        el = getEl(collideField, x, y, z, 0, xlength);
+        computeDensity(el, &density);
+        computeVelocity(el, &density, velocity);
 
-    fprintf(fp,"\n");
+        fprintf(fp, "%f %f %f\n", velocity[0], velocity[1], velocity[2]);
+      }
+    }
+  }
+
+  fprintf(fp,"\n");
   
-    fprintf(fp, "SCALARS density float 1 \n"); 
-    fprintf(fp, "LOOKUP_TABLE default \n");
+  fprintf(fp, "SCALARS density float 1 \n"); 
+  fprintf(fp, "LOOKUP_TABLE default \n");
 
-    for(z = 0; z < n; z++) {
-        for(y = 0; y < n; y++) {
-            for(x = 0; x < n; x++) {
-                /* TODO compute dencity */
-                dencity = 1;
-                fprintf(fp, "%f\n", dencity);
-            }
-        }
+  for(z = 0; z < n; z++) {
+    for(y = 0; y < n; y++) {
+      for(x = 0; x < n; x++) {
+        /* TODO compute density */
+        computeDensity(getEl(collideField, x, y, z, 0, xlength), &density);
+        fprintf(fp, "%f\n", density);
+      }
     }
+  }
 
-    if( fclose(fp) )
+  if( fclose(fp) )
     {
-        char szBuff[80];
-        sprintf( szBuff, "Failed to close %s", szFileName );
-        ERROR( szBuff );
+      char szBuff[80];
+      sprintf( szBuff, "Failed to close %s", szFileName );
+      ERROR( szBuff );
     }
 }
 
 
 void write_vtkHeader( FILE *fp, int xlength) {
-    int n = xlength + 2;
+  int n = xlength + 2;
 
   if( fp == NULL ) {
     char szBuff[80];
@@ -89,26 +93,26 @@ void write_vtkHeader( FILE *fp, int xlength) {
 
 
 void write_vtkPointCoordinates(FILE *fp, int xlength) {
-    int originX = 0;
-    int originY = 0;
-    int originZ = 0;
+  int originX = 0;
+  int originY = 0;
+  int originZ = 0;
 
-    int x = 0;
-    int y = 0;
-    int z = 0;
-    int n = xlength + 2;
+  int x = 0;
+  int y = 0;
+  int z = 0;
+  int n = xlength + 2;
 
-    for(z = 0; z < n; z++) {
-        for(y = 0; y < n; y++) {
-            for(x = 0; x < n; x++) {
-                /* dx = dy = dz = 1 */
-                fprintf(fp, "%d %d %d\n", originX + x, originY + y, originZ + z);
-            }
-        }
+  for(z = 0; z < n; z++) {
+    for(y = 0; y < n; y++) {
+      for(x = 0; x < n; x++) {
+        /* dx = dy = dz = 1 */
+        fprintf(fp, "%d %d %d\n", originX + x, originY + y, originZ + z);
+      }
     }
+  }
 }
 
 void writeVtkOutput(const double * const collideField, const int * const flagField, const char * filename, unsigned int t, int xlength) {
-    write_vtkFile("ws2", t, xlength, collideField);
+  write_vtkFile("ws2", t, xlength, collideField);
 }
 
