df321f23ad25dd21e797729bad5d49e7f7fcde1b
irvcaza <irvcaza@gmail.com>
2016-06-25 16:13:10 +0200

    Small changes Normal DF reconstruction
    Still not working so its commented

--- project/collision.c
+++ project/collision.c
@@ -19,9 +19,12 @@ void computeNormal(int* node, int* length, double * fractionField, double* norma
         node_back[i] --;
         node_front[i] ++;
         normal_dir[i] = (*getFraction(fractionField, node_back, n) - *getFraction(fractionField, node_front, n))/2;
+        //printf("%e %e , ", *getFraction(fractionField, node_back, n) , *getFraction(fractionField, node_front, n));
+        //printf ("%d %d %d , %d %d %d , %d %d %d \n", node_back[0],node_back[1],node_back[2],node[0],node[1],node[2],node_front[0],node_front[1],node_front[2]);
         node_back[i] = node[i];
         node_front[i] = node[i];
     }
+    //printf("\n");
 }
 
 double computeExternal (int i, double density, double * extForces){
@@ -35,33 +38,36 @@ void computePostCollisionDistributions(int *node, double * currentCell, int* fla
 
     int i;
     double fi;
-    // int n[3] = { length[0] + 2, length[1] + 2, length[2] + 2 };
+    int n[3] = { length[0] + 2, length[1] + 2, length[2] + 2 };
 
-    // if (*getFlag(flagField, node, n) == INTERFACE){
-    //     int source_node[D];
-    //     double normal_dir[3];
+    if (*getFlag(flagField, node, n) == INTERFACE){        
+        // int source_node[D];
+        double normal_dir[3];
+        // double epsilon = 0.00000000001;
 
-    //     for (i=0; i<Q; i++) {
-    //         source_node[0] = node[0] - LATTICEVELOCITIES[i][0];
-    //         source_node[1] = node[1] - LATTICEVELOCITIES[i][1];
-    //         source_node[2] = node[2] - LATTICEVELOCITIES[i][2];
-    //         computeNormal(node, length, fractionField, normal_dir);
+        for (i=0; i<Q; i++) {
+            /* Make this part with the normak work, folowing eq 4.5 in PHD */
+            // source_node[0] = node[0] + LATTICEVELOCITIES[i][0];
+            // source_node[1] = node[1] + LATTICEVELOCITIES[i][1];
+            // source_node[2] = node[2] + LATTICEVELOCITIES[i][2];
+            computeNormal(node, length, fractionField, normal_dir);
 
-    //         if (*getFlag(flagField, source_node, n) == GAS || dotProd(LATTICEVELOCITIES[i],normal_dir,D) >0 ){
-    //             currentCell[Q-i] = feqAtm[i] + feqAtm[Q-i] - currentCell[i];
-    //         }
-    //         else{
-    //             fi = currentCell[i];
-    //             currentCell[i] = fi - (fi - feq[i]) / *tau + computeExternal(i, density, extForces);
-    //         }
-    //     }            
-    // }
-    // else{
+            // if (*getFlag(flagField, source_node, n) == GAS || dotProd(LATTICEVELOCITIES[i],normal_dir,D) > epsilon ){
+            //     printf("Node %d %d %d, latttice %d %d %d, GasDest %d, normal: %e %e %e , diotprod %e vs %e \n",node[0],node[1],node[2],LATTICEVELOCITIES[i][0],LATTICEVELOCITIES[i][1],LATTICEVELOCITIES[i][2], *getFlag(flagField, source_node, n) ==GAS, normal_dir[0],normal_dir[1],normal_dir[2], dotProd(LATTICEVELOCITIES[i],normal_dir,D), epsilon );
+            //     currentCell[Q-i] = feqAtm[i] + feqAtm[Q-i] - currentCell[i];
+            // }
+            // else{
+               fi = currentCell[i];
+               currentCell[i] = fi - (fi - feq[i]) / *tau + computeExternal(i, density, extForces);
+            // }
+        }            
+    }
+    else{
         for (i=0; i<Q; i++) {
             fi = currentCell[i];
             currentCell[i] = fi - (fi - feq[i]) / *tau + computeExternal(i, density, extForces);
         }
-    // }
+    }
 }
 
 void doCollision(double *collideField, int *flagField, double * massField, double * fractionField, const double * const tau, int * length, double * extForces){
@@ -94,7 +100,7 @@ void doCollision(double *collideField, int *flagField, double * massField, doubl
                     computeVelocity(currentCell, &density, velocity);
                     computeFeq(&density, velocity, feq);
                     computeFeq(&densityAtm, velocity, feqAtm);
-                    computePostCollisionDistributions(node, currentCell, flagField, fractionField, tau, feq, feqAtm, density, extForces, n);
+                    computePostCollisionDistributions(node, currentCell, flagField, fractionField, tau, feq, feqAtm, density, extForces, length);
 
                     /* Update fluid fraction */
                     *getFraction(fractionField, node, n) = *getMass(massField, node, n) / density;
